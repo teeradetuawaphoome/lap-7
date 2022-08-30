901b4cd5205b511af243ec750cd50bc5d4db6231
teeradetuawaphoome <108716921+teeradetuawaphoome@users.noreply.github.com>
2022-08-30 13:03:37 +0700

    tew1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,11 +11,49 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Widget myButton() {
+  String result = "0";
+  String expressions = "";
+  buttonPressed(String value) {
+    print(value);
+
+    setState(() {
+      if (value == "CLEAR") {
+        result = "0";
+      } else if (value == ".") {
+        if (result.contains(".")) {
+          return;
+        } else {
+          result = result + value;
+        }
+      } else if (value == "=") {
+        expressions = result.replaceAll("x", "*");
+
+        Parser p = Parser();
+        Expression exp = p.parse(expressions);
+        ContextModel cm = ContextModel();
+        dynamic calculate = exp.evaluate(EvaluationType.REAL, cm);
+
+        result = "$calculate";
+      } else {
+        if (result == "0") {
+          result = value;
+        } else {
+          result = result + value;
+        }
+      }
+    });
+  }
+
+  Widget myButton(String buttonLabel) {
     return Expanded(
       child: OutlinedButton(
-        onPressed: () {},
-        child: Text("XXX"),
+        onPressed: () => buttonPressed(buttonLabel),
+        child: Text(
+          buttonLabel,
+          style: TextStyle(
+            fontSize: 20,
+          ),
+        ),
       ),
     );
   }
@@ -29,20 +68,57 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Row(
-              children: [
-                myButton(),
-                myButton(),
-                myButton(),
-                myButton(),
-              ],
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 12, vertical: 24),
+              alignment: Alignment.centerRight,
+              child: Text(
+                result,
+                style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
+              ),
             ),
-            Row(
+            Expanded(
+              child: Divider(),
+            ),
+            Column(
               children: [
-                myButton(),
-                myButton(),
-                myButton(),
-                myButton(),
+                Row(
+                  children: [
+                    myButton("7"),
+                    myButton("8"),
+                    myButton("9"),
+                    myButton("/"),
+                  ],
+                ),
+                Row(
+                  children: [
+                    myButton("4"),
+                    myButton("5"),
+                    myButton("6"),
+                    myButton("x"),
+                  ],
+                ),
+                Row(
+                  children: [
+                    myButton("1"),
+                    myButton("2"),
+                    myButton("3"),
+                    myButton("-"),
+                  ],
+                ),
+                Row(
+                  children: [
+                    myButton("."),
+                    myButton("0"),
+                    myButton("00"),
+                    myButton("+"),
+                  ],
+                ),
+                Row(
+                  children: [
+                    myButton("CLEAR"),
+                    myButton("="),
+                  ],
+                ),
               ],
             ),
           ],
